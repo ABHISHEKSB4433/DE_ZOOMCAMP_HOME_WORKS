9bb238fba58aedd7a543c7ba0c85db779aa590ee
ABHISHEKSB4433 <93099817+ABHISHEKSB4433@users.noreply.github.com>
2022-02-14 18:29:25 +0530

    Create WEEK_3_HW.py

---
+++ WEEK_3_HW.py
@@ -0,0 +1,27 @@
+#Question 1: What is count for fhv vehicles data for year 2019
+
+#ans = 
+
+
+#Question 2: How many distinct dispatching_base_num we have in fhv for 2019 
+
+
+
+#Question 3: Best strategy to optimise if query always filter by dropoff_datetime and order by dispatching_base_num 
+
+
+#Question 4: What is the count, estimated and actual data processed for query which counts trip between 2019/01/01 and 2019/03/31 for dispatching_base_num B00987, B02060, B02279 
+
+
+
+#Question 5: What will be the best partitioning or clustering strategy when filtering on dispatching_base_num and SR_Flag
+
+
+
+
+#Question 6: What improvements can be seen by partitioning and clustering for data size less than 1 GB 
+
+
+
+#Question 7: In which format does BigQuery save data
+# ans = Bigquery saves data in columnar
