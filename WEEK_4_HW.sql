2604ec2d203c21b576c8c439c19f2fb4d4ae51a5
ABHISHEKSB4433 <93099817+ABHISHEKSB4433@users.noreply.github.com>
2022-02-24 01:56:16 +0530

    Create WEEK_4_HW.sql

---
+++ WEEK_4_HW.sql
@@ -0,0 +1,14 @@
+# Question 1: What is the count of records in the model fact_trips after running all models with the test run variable disabled and filtering for 2019 and 2020 data only (pickup datetime) *
+
+SELECT * FROM 
+
+#Question 2: What is the distribution between service type filtering by years 2019 and 2020 data as done in the videos . (Yellow/Green) *
+
+
+#Question 3: What is the count of records in the model stg_fhv_tripdata after running all models with the test run variable disabled *
+
+
+#Question 4: What is the count of records in the model fact_fhv_trips after running all dependencies with the test run variable disabled *
+
+
+#Question 5: What is the month with the biggest amount of rides after building a tile for the fact_fhv_trips table *
